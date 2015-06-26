ec78d8d69ad338d2d03bb3aeec97781a2373e7fe
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-06-26 15:36:10 -0400

    Adding snippets install

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 https://github.com/twbs/bootstrap/releases/download/v3.3.5/bootstrap-3.3.5-dist.zip
 wget http://code.jquery.com/jquery-2.1.4.min.js
 mv jquery-2.1.4.min.js scripts
+wget https://visualstudiogallery.msdn.microsoft.com/e82e7862-f731-4183-a27a-3a44b261bfe5/file/135904/11/BootstrapSnippets.vsix
